71f8acbeb4b036ac24ee469b8acc352b0e0babbd
Tony-mon <tonio@inbox.ru>
2014-09-24 17:40:40 +0400

    Add Template

---
+++ Template/include/header.php
@@ -0,0 +1,16 @@
+<? include 'include/head.php'; ?>
+<header id="header">
+	<div id="containerHeader" class="inner">
+		<a href="" class="logo"><img src="i/logo.jpg" alt=""></a>
+	</div>
+</header>
+<div id="menu">
+	<nav class="inner nav">
+		<li>
+			<a href="">Меню</a>
+		</li>
+	</nav>
+</div>
+<div id="containerMain" >
+	<div class="containerMain inner">	
+		<main role="main">
